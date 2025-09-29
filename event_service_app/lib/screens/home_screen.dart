fa46a289a4e4e49598e6cc143680bb11d31fdcca
Anas Mohd Haslan <anashaslan@gmail.com>
2025-09-29 08:55:28 +0800

    Fixed home screen where after vendor login, will direct to vendor dashboard, not directly to registration services.

--- event_service_app/lib/screens/home_screen.dart
+++ event_service_app/lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:event_service_app/screens/vendor_home.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -49,7 +50,7 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: _userRole == 'customer'
           ? CustomerBrowseServicesScreen()
-          : VendorServiceRegistrationScreen(),
+          : VendorHomeScreen(),
     );
   }
 }
